79a4f8866faacc913351c26cd1b6a9b2aa586bc9
Dan S <dan@shoeman-tech.com>
2026-08-10 21:21:16 -0700

    updated Alternate Portraits Lunna Raphael to SMAPI 4.5.2 and CP 2.9.1

--- SafeLightning/ModConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shockah.SafeLightning
-{
-	internal class ModConfig
-	{
-		public bool SafeTiles { get; set; } = true;
-		public bool SafeFruitTrees { get; set; } = true;
-		public BigLightningBehavior BigLightningBehavior { get; set; } = BigLightningBehavior.WhenSupposedToStrike;
-	}
-}
